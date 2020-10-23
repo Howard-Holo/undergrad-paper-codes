0aa541c478b4b0ff0727c208b2f752feff18f407
Howard Holo <howard.holo@outlook.com>
2020-10-23 21:18:09 +0800

    Update scrapy_liniannianbao.py

--- data_fetch/scrapy_liniannianbao.py
+++ data_fetch/scrapy_liniannianbao.py
@@ -5,9 +5,9 @@ import csv                          # 用于csv文件的读取和写入
 import shutil, os, random
 
 userAgentDic = {
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit %s (KHTML, like Gecko) Chrome" % round(random.uniform(0,999.99),2),
-    "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/%s (KHTML, like Gecko) Version/5.1 Safari" % round(random.uniform(0,999.99),2),
-    "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-us) AppleWebKit/%s (KHTML, like Gecko) Version/5.1 Safari" % round(random.uniform(0,999.99),2),
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit %s (KHTML, like Gecko) Chrome" % round(random.uniform(0.,999.99),2),
+    "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/%s (KHTML, like Gecko) Version/5.1 Safari" % round(random.uniform(0.,999.99),2),
+    "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-us) AppleWebKit/%s (KHTML, like Gecko) Version/5.1 Safari" % round(random.uniform(0.,999.99),2),
     "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0",
     "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0)",
     "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)",
